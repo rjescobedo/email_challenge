f33922a5da1bf9eb9fdf2e6e8b021bf60e6d560b
Raul Escobedo <rjescobedo@gmail.com>
2022-04-24 06:06:34 -0500

    Added JS

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+//Track Selected Elements
+const arrayWithElements = new Array();
+
+function clickListener(e) {   
+    const clickedElement = (window.event)
+                        ? window.event.srcElement
+                        : e.target,
+        tags = document.getElementsByTagName(clickedElement.tagName);
+    for(let i=0; i<tags.length; ++i) {
+      
+        if(tags[i] == clickedElement) {
+        arrayWithElements.push({tag:clickedElement.tagName, index:i}); 
+        console.log(arrayWithElements);
+      } 
+    }
+}
+document.onclick = clickListener;
+
+
+
+
+
