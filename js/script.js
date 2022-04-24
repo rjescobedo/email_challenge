12efaff6825cc23f4f0c621d84df22ed074d328c
Raul Escobedo <rjescobedo@gmail.com>
2022-04-24 17:20:26 -0500

    4/24 deployment

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
-
 //Track Selected Elements
-// const arrayWithElements = new Array();
+const arrayWithElements = new Array();
 
-// function clickListener(e) {   
-//     const clickedElement = (window.event)
-//                         ? window.event.srcElement
-//                         : e.target,
-//         tags = document.getElementsByTagName(clickedElement.tagName);
-//     for(let i = 0; i<tags.length; ++i) {
+function clickListener(e) {   
+    const clickedElement = (window.event)
+                        ? window.event.srcElement
+                        : e.target,
+        tags = document.getElementsByTagName(clickedElement.tagName);
+    for(let i = 0; i<tags.length; ++i) {
       
-//         if(tags[i] == clickedElement) {
-//         arrayWithElements.push({tag:clickedElement.tagName, index:i}); 
-//         console.log(arrayWithElements);
-//       } 
-//     }
-// }
-// document.onclick = clickListener;
+        if(tags[i] == clickedElement) {
+        arrayWithElements.push({tag:clickedElement.tagName, index:i}); 
+        console.log(arrayWithElements);
+      } 
+    }
+}
+document.onclick = clickListener;
 
 
 
